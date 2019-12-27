e2182bcf55336e0cdff6d3a32dc8fac50c06b822
Locwilder <loc.nguyen@outlook.com>
2019-12-27 17:44:05 +0100

    conf

--- src/main/java/main/java/com/locspring/springit/SpringitApplication.java
+++ src/main/java/main/java/com/locspring/springit/SpringitApplication.java
@@ -1,13 +1,30 @@
 package main.java.com.locspring.springit;
 
+import com.config.SpringitProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SpringitProperties.class)
 public class SpringitApplication {
+
+    @Autowired
+    private SpringitProperties springitProperties;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
     }
+
+    @Bean
+    CommandLineRunner runner(){
+        return args -> {
+            System.out.println("this is our message " + springitProperties.getWelcomeMsg());
+        };
+    }
+
 
 }
