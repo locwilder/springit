8a42cc0e2b7b142c058d198cda6a6e8e7b6ec716
Locwilder <loc.nguyen@outlook.com>
2019-12-30 18:40:03 +0100

    created entities and refactored with lombok

---
+++ src/main/java/main/java/com/locspring/springit/domain/Comment.java
@@ -0,0 +1,20 @@
+package main.java.com.locspring.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String body;
+
+}
