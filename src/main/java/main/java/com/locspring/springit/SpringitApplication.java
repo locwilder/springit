3920b51c595fdcf45b6ee09e0ed6051cef174c56
Locwilder <dc_fou@hotmail.com>
2020-01-04 01:11:42 +0100

    security part 2 added login and register templates

--- src/main/java/main/java/com/locspring/springit/SpringitApplication.java
+++ src/main/java/main/java/com/locspring/springit/SpringitApplication.java
@@ -16,6 +16,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 import javax.swing.*;
 
@@ -32,6 +33,13 @@ public class SpringitApplication {
     @Bean
     PrettyTime prettyTime() {
         return new PrettyTime();
+    }
+
+    // TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
+// TODO   for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extras-springsecurity4)
+    @Bean
+    public SpringSecurityDialect securityDialect() {
+        return new SpringSecurityDialect();
     }
 
     /*@Autowired
--- src/main/java/main/java/com/locspring/springit/security/SecurityConfiguration.java
+++ src/main/java/main/java/com/locspring/springit/security/SecurityConfiguration.java
@@ -30,9 +30,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/h2-console/**").permitAll()
                 .and()
                 .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .usernameParameter("email")
                 .and()
-                .csrf().disable()
-                .headers().frameOptions().disable();
+                .logout()
+                .and()
+                .rememberMe();
+//                .and()
+//                .csrf().disable()
+//                .headers().frameOptions().disable();
     }
 
     @Override
